7d0133ad6f8e0d4dc0d5e94ed826420d89d477a7
Baptiste Mathus <batmat@batmat.net>
2016-11-13 23:40:17 +0100

    Update output names for new machines
    Yeah, I should script that and NOT hardcode that...

--- roles/_personal/files/xrandr-reset-LVDS.sh
+++ roles/_personal/files/xrandr-reset-LVDS.sh
@@ -4,11 +4,11 @@ export DISPLAY=:0
 
 if [[ $( xrandr  | grep " connected" | wc -l ) -le 1 ]]; then
   #echo "Aucun autre écran allumé, allumage LCD et off des autres"
-  xrandr --output LVDS1 --auto
-  xrandr --output HDMI1 --off
-  xrandr --output HDMI2 --off
-  xrandr --output HDMI3 --off
-  xrandr --output VGA1 --off
+  xrandr --output eDP-1 --auto
+  xrandr --output DP-1 --off
+  xrandr --output HDMI-1 --off
+  xrandr --output DP-2 --off
+  xrandr --output HDMI-2 --off
 else
   echo "Autres écrans allumés"
 fi
