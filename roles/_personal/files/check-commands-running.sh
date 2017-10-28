93912f2bbe4ae0037953d92d965f930cafee25a9
Baptiste Mathus <batmat@batmat.net>
2017-10-28 14:45:06 +0200

    Add script & cron to check for running processes
    Some processes I always want to be running. And sometimes they don't
    for whatever reason, fix that.

---
+++ roles/_personal/files/check-commands-running.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -euo pipefail
+
+# This script checks if a given list of command is running or not, and notifies if not
+
+# unsetting PATH to be closer to cron env/behaviour
+unset PATH
+export PATH=/bin
+export DISPLAY=:0
+
+
+export COMMANDS_TO_CHECK="rescuetime copyq"
+
+notify ()
+{
+  /usr/bin/notify-send -t 3000 "Missing process: $1" "$1 is expected to always be running, but is missing, check why!"
+}
+
+for cmd in $COMMANDS_TO_CHECK
+do
+  if ! pgrep $cmd ; then
+    notify $cmd
+  fi
+done
